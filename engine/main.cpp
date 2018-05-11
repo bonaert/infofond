50b61a64025e43eb60d43c5d23977b729d48d367
Gregory Bonaert <harokb@gmail.com>
2018-05-11 19:57:11 +0200

    update

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,5 +1,6 @@
 #include "graph.hpp"
 #include "../solver/Solver.hpp"
+#include <iostream>
 
 
 #define TIMEWAIT 3			// le temps d'attente minimum dans une gare desservie
@@ -7,7 +8,7 @@
 #define FAST 0				// nombre de train rapide
 #define TRAIN (SLOW+FAST)	// nombre de train
 #define TIMESLOT 30			// nombre de minutes dans la plage horaire
-#define STATION 4			// nombre de gare (voir la map)
+#define STATION 3			// nombre de gare (voir la map)
 #define TIMEDURATION 5		// duree maximal des voyages direct
 #define TIMEWINDOW 5		// frequence des trains direct
 
@@ -47,8 +48,25 @@ int main() {
 	map->print();
 	assert(STATION == map->get_size());
 
+	// ---------- Outputs ---------- //
+
+	std::cout << "The number of slow trains is " << SLOW << std::endl;
+	std::cout << "The number of fast trains is " << FAST << std::endl;
+
+	for (int i = 0; i < STATION; ++i)
+	{
+		std::cout << "Station " << map->get_name(i) << " " << i << " with capacity " << map->get_capacity(i) << std::endl;
+		for (int j = 0; j < STATION; ++j)
+		{
+			if (map->get_duration(i, j) > 0) {
+				std::cout << "    Distance from " << i << " to " << j << " is " << map->get_duration(i, j) << std::endl;
+			}
+		}
+	}
 
 	// ---------- Variables ---------- //
+
+	
 
 	init_varA();
 	init_varB();
